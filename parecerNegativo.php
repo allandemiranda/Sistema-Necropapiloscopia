11327dee07244234cd063c8d01bafc7ef2a6127a
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-07-16 18:40:38 -0300

    novo modelo parecer

--- parecerNegativo.php
+++ parecerNegativo.php
@@ -5,77 +5,128 @@
     <title>Sistema Necropapiloscopia </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="Sistema, documentos" />
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <style>
+        .A4 {
+            background-color: #ffffff;
+            color: #000000;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 15pt;
+            line-height: 1.3;
+        }
+
+        .cabecalho-texto {
+            text-align: center;
+            font-size: 15pt;
+        }
+
+        .cabecalho-img-esquerda {
+            float: left;
+            width: 100%;
+        }
+
+        .cabecalho-img-direita {
+            float: right;
+            width: 100%;
+        }
+
+        .titulo {
+            text-align: center;
+            font-size: 16pt;
+            color: #000000;
+            background-color: #d5e2bb;
+        }
+
+        .paragrafo {
+            text-indent: 4em;
+            text-align: justify;
+        }
+
+        .datacao {
+            text-align: right;
+        }
+
+        .assinatura {
+            text-align: center;
+            line-height: 0.2;
+        }
+    </style>
 </head>
 
-<body style="background-color: #ffffff; max-width: 481.9pt; color: #000000; line-height: 1.0;">
-    <div class="col-md-12" style="margin-top: 10px;">
-        <div class="col-md-2"><img src="images/logoRN.jpg" style="width: 100%;" /></div>
-        <div class="col-md-8" style="text-align: center;">
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>Governo do Estado do Rio Grande do Norte</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>Secretaria de Estado da Segurança Pública e da
-                    Defesa Social</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>INSTITUTO TÉCNICO-CIENTÍFICO DE PERÍCIA - ITEP</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>INSTITUTO DE IDENTIFICAÇÃO - II</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <i>Setor de Necropapiloscopia</i>
-            </p>
+<body class="A4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2"><img class="cabecalho-img-esquerda" src="images/logoRN.jpg" /></div>
+            <div class="col-sm-8 cabecalho-texto">
+                <div><b>Governo do Estado do Rio Grande do Norte</b></div>
+                <div><b>Secretaria de Estado da Segurança Pública e da Defesa Social</b></div>
+                <div style="font-size: 16pt;"><b>INSTITUTO TÉCNICO-CIENTÍFICO DE PERÍCIA - ITEP</b></div>
+                <div style="font-size: 16pt;"><b>INSTITUTO DE IDENTIFICAÇÃO - II</b></div>
+                <div style="font-size: 16pt;"><i>Setor de Necropapiloscopia</i></div>
+            </div>
+            <div class="col-sm-2"><img class="cabecalho-img-direita" src="images/logoITEP.jpg" /></div>
         </div>
-        <div class="col-md-2"><img src="images/logoITEP.jpg" style="width: 100%;" /></div>
-    </div>
-    <div class="col-md-12" style="margin-top: 34px;">
-        <div class="col-md-1"></div>
-        <div class="col-md-10 alert alert-success alert-dismissable"
-            style="font-size: 11pt; font-family: 'Times New Roman'; background-color: #d5e2bb; color: #000000; text-align: center;">
-            <b>INFORMAÇÃO TÉCNICA NECROPAPILOSCÓPICA Nº 0000/0000</b>
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-sm-10 titulo alert alert-success">
+                <b>INFORMAÇÃO TÉCNICA NECROPAPILOSCÓPICA Nº 0000/0000</b>
+            </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="row">
+            <div class="col-sm-12 paragrafo">
+                <p>
+                    Em <b>02 de Junho de 2019</b> , o Instituto de Identificação do Rio Grande do Norte - IIRN, em
+                    conformidade com a legalidade vigente na Portaria nº 119/2012- GS/SESED informa que, foram
+                    realizadas coletas das impressões digitais no cadáver registrado no Instituto de Medicina Legal -
+                    IML sob <b>NIC 00967</b>, uma criança do sexo feminino, procedente de Bairro Nísia Floresta RN e não
+                    foi submetido a Exame de Confronto Necropapiloscópico por não apresentarem até o momento, documentos
+                    com padrões dactiloscópicos que confirmem a Identidade Civil do(a) mesmo(a). Foram realizados
+                    pesquisas no nosso banco de dados civil e não foi localizado nenhum registro referente ao citado
+                    cadáver.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 paragrafo">
+                <p>
+                    As planilhas dactiloscópicas coletadas e não identificadas serão arquivadas no Setor de
+                    Necropapiloscopia para que se dê continuidade a futuras pesquisas.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 paragrafo">
+                <p>
+                    Diante do exposto o signatário informa que o cadáver, <b>NIC 00967 <u style="color: red">NÃO FOI
+                            IDENTIFICADO(A) PELO SISTEMA DACTILOSCÓPICO.</u></b>
+                </p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col datacao">
+                <p>Natal/RN, 00/00/0000</p>
+            </div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col assinatura">
+                <p><b>NOME DA PESSOA AQUI</b></p>
+                <p>Cargo da pessoa aqui</p>
+                <p>Mat 00.00-0</p>
+            </div>
+        </div>
     </div>
-    <div class="col-md-12"
-        style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-12" style="text-indent:4em">
-            Em <b>02 de Junho de 2019</b> , o Instituto de Identificação do Rio Grande do Norte - IIRN, em conformidade
-            com a legalidade vigente na Portaria nº 119/2012- GS/SESED informa que, foram realizadas coletas das
-            impressões digitais no cadáver registrado no Instituto de Medicina Legal - IML sob <b>NIC 00967</b>, uma
-            criança do sexo feminino, procedente de Bairro Nísia Floresta RN e não foi submetido a Exame de Confronto
-            Necropapiloscópico por não apresentarem até o momento, documentos com padrões dactiloscópicos que confirmem
-            a Identidade Civil do(a) mesmo(a). Foram realizados pesquisas no nosso banco de dados civil e não foi
-            localizado nenhum registro referente ao citado cadáver.
-        </p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-12" style="text-indent:4em">
-            As planilhas dactiloscópicas coletadas e não identificadas serão arquivadas no Setor de Necropapiloscopia
-            para que se dê continuidade a futuras pesquisas.
-        </p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-12" style="text-indent:4em">
-            Diante do exposto o signatário informa que o cadáver, <b>NIC 00967 <u style="color: red">NÃO FOI IDENTIFICADO (A) PELO SISTEMA DACTILOSCÓPICO.</u></b>
-        </p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 54px; font-size: 11pt; font-family: 'Times New Roman'; text-align: right;">
-        <p class="col-md-12">Natal/RN, 00 de janeiro de 0000</p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 34px; font-size: 11pt; font-family: 'Times New Roman'; text-align: center;">
-        <p><b>NOME DA PESSOA AQUI</b></p>
-        <p>Cargo da pessoa aqui</p>
-        <p>Mat 00.00-0</p>
-    </div>
-
 </body>
 
 </html>
--- parecerPositivo.php
+++ parecerPositivo.php
@@ -5,87 +5,174 @@
     <title>Sistema Necropapiloscopia </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="Sistema, documentos" />
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <style>
+        .A4 {
+            background-color: #ffffff;
+            color: #000000;
+            font-family: 'Times New Roman', Times, serif;
+            font-size: 15pt;
+            line-height: 1.3;
+        }
+
+        .cabecalho-texto {
+            text-align: center;
+            font-size: 15pt;
+        }
+
+        .cabecalho-img-esquerda {
+            float: left;
+            width: 100%;
+        }
+
+        .cabecalho-img-direita {
+            float: right;
+            width: 100%;
+        }
+
+        .titulo {
+            text-align: center;
+            font-size: 16pt;
+            color: #000000;
+            background-color: #d5e2bb;
+        }
+
+        .paragrafo {
+            text-indent: 4em;
+            text-align: justify;
+        }
+
+        .datacao {
+            text-align: right;
+        }
+
+        .assinatura {
+            text-align: center;
+            line-height: 0.2;
+        }
+    </style>
 </head>
 
-<body style="background-color: #ffffff; max-width: 481.9pt; color: #000000; line-height: 1.0;">
-    <div class="col-md-12" style="margin-top: 10px;">
-        <div class="col-md-2"><img src="images/logoRN.jpg" style="width: 100%;" /></div>
-        <div class="col-md-8" style="text-align: center;">
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>Governo do Estado do Rio Grande do Norte</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>Secretaria de Estado da Segurança Pública e da
-                    Defesa Social</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>INSTITUTO TÉCNICO-CIENTÍFICO DE PERÍCIA - ITEP</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <b>INSTITUTO DE IDENTIFICAÇÃO - II</b>
-            </p>
-            <p style="font-size: 10pt; font-family: 'Times New Roman'; ">
-                <i>Setor de Necropapiloscopia</i>
-            </p>
+<body class="A4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2"><img class="cabecalho-img-esquerda" src="images/logoRN.jpg" /></div>
+            <div class="col-sm-8 cabecalho-texto">
+                <div><b>Governo do Estado do Rio Grande do Norte</b></div>
+                <div><b>Secretaria de Estado da Segurança Pública e da Defesa Social</b></div>
+                <div style="font-size: 16pt;"><b>INSTITUTO TÉCNICO-CIENTÍFICO DE PERÍCIA - ITEP</b></div>
+                <div style="font-size: 16pt;"><b>INSTITUTO DE IDENTIFICAÇÃO - II</b></div>
+                <div style="font-size: 16pt;"><i>Setor de Necropapiloscopia</i></div>
+            </div>
+            <div class="col-sm-2"><img class="cabecalho-img-direita" src="images/logoITEP.jpg" /></div>
         </div>
-        <div class="col-md-2"><img src="images/logoITEP.jpg" style="width: 100%;" /></div>
-    </div>
-    <div class="col-md-12" style="margin-top: 34px;">
-        <div class="col-md-1"></div>
-        <div class="col-md-10 alert alert-success alert-dismissable"
-            style="font-size: 11pt; font-family: 'Times New Roman'; background-color: #d5e2bb; color: #000000; text-align: center;">
-            <b>INFORMAÇÃO TÉCNICA NECROPAPILOSCÓPICA Nº 0000/0000</b>
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-sm-10 titulo alert alert-success">
+                <b>INFORMAÇÃO TÉCNICA NECROPAPILOSCÓPICA Nº 0000/0000</b>
+            </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="row">
+            <div class="col-sm-12 paragrafo">
+                <p>
+                    Em, <b>09 de Julho de 2019</b> o Instituto de Identificação do Rio Grande do Norte - IIRN, em
+                    conformidade com a legalidade vigente na Portaria nº 119/2012 - GS/SESED informa que, após coletas
+                    das
+                    impressões digitais no cadáver registrado no Instituto de Medicina Legal - IML sob <b>NIC 04381</b>
+                    foram submetidos a Exame de Confronto Necropapiloscópico com as impressões digitais provenientes
+                    do(a), <b>RG 1.664.730 SSP/RN</b> conforme dados qualificativos a seguir:
+                </p>
+            </div>
+        </div>
+        <div class="row" style="line-height: 0.2;">
+            <div class="col-sm-4">
+                <p><b>DATA DE ENTRADA:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>09/07/2019</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>PROCEDÊNCIA:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>BAIRRO CIDADE UF</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>NOME COMPLETO:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>MAELDE ROSA DE ALMEIDA</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>NOME DO PAI:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>LUIZ MARTINS DA SILVA</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>NOME DA MÃE:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>ALZENIR ESTEVAO DE ALMEIDA</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>NATURALIDADE:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>CAMPO GRANDE RN</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>DATA DE NASCIMENTO:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>15/01/1978</p>
+            </div>
+            <div class="col-sm-4">
+                <p><b>DOC. APRESENTADO:</b></p>
+            </div>
+            <div class="col-sm-8">
+                <p>RG 1.664.730 SSP RN</p>
+            </div>
+            <div class="col-sm-4" style="color: red">
+                <p><b><u>OBSERVAÇÃO:</u></b></p>
+            </div>
+            <div class="col-sm-8" style="color: red">
+                <p>ENFORCAMENTO</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 paragrafo">
+                <p>
+                    Diante dos exames realizados o signatário conclui que <u><b>foram encontrados padrões
+                            dactiloscópicos que confirmam a Identidade Civil do mesmo.</b></u>
+                </p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col datacao">
+                <p>Natal/RN, 00/00/0000</p>
+            </div>
+        </div>
+        <br>
+        <div class="row ">
+            <div class="col assinatura">
+                <p><b>NOME DA PESSOA AQUI</b></p>
+                <p>Cargo da pessoa aqui</p>
+                <p>Mat 00.00-0</p>
+            </div>
+        </div>
     </div>
-    <div class="col-md-12" style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-12" style="text-indent:4em">
-            Em, <b>09 de Julho de 2019</b> o Instituto de Identificação do Rio Grande do Norte - IIRN, em conformidade
-            com a legalidade vigente na Portaria nº 119/2012 - GS/SESED informa que, após coletas das impressões
-            digitais no cadáver registrado no Instituto de Medicina Legal - IML sob <b>NIC 04381</b> foram submetidos a
-            Exame de Confronto Necropapiloscópico com as impressões digitais provenientes do(a), <b>RG 1.664.730
-                SSP/RN</b> conforme dados qualificativos a seguir:
-        </p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-6"><b>DATA DE ENTRADA:</b></p>
-        <p class="col-mf-6">09/07/2019</p>
-        <p class="col-md-6"><b>PROCEDÊNCIA:</b></p>
-        <p class="col-mf-6">BAIRRO CIDADE UF</p>
-        <p class="col-md-6"><b>NOME COMPLETO:</b></p>
-        <p class="col-mf-6">MAELDE ROSA DE ALMEIDA</p>
-        <p class="col-md-6"><b>NOME DO PAI:</b></p>
-        <p class="col-mf-6">LUIZ MARTINS DA SILVA</p>
-        <p class="col-md-6"><b>NOME DA MÃE:</b></p>
-        <p class="col-mf-6">ALZENIR ESTEVAO DE ALMEIDA</p>
-        <p class="col-md-6"><b>NATURALIDADE:</b></p>
-        <p class="col-mf-6">CAMPO GRANDE RN</p>
-        <p class="col-md-6"><b>DATA DE NASCIMENTO:</b></p>
-        <p class="col-mf-6">15/01/1978</p>
-        <p class="col-md-6"><b>DOC. APRESENTADO:</b></p>
-        <p class="col-mf-6">RG 1.664.730 SSP RN</p>
-        <p class="col-md-6" style="color: red"><b>OBSERVAÇÃO:</b></p>
-        <p class="col-mf-6" style="color: red">ENFORCAMENTO</p>
-    </div>
-    <div class="col-md-12"
-        style="margin-top: 14px; font-size: 11pt; font-family: 'Times New Roman'; text-align: justify;">
-        <p class="col-md-12" style="text-indent:4em">Diante dos exames realizados o signatário conclui que <u><b>foram encontrados padrões dactiloscópicos que confirmam
-            a Identidade Civil do mesmo.</b></u></p>
-    </div>
-    <div class="col-md-12"
-    style="margin-top: 54px; font-size: 11pt; font-family: 'Times New Roman'; text-align: right;">
-        <p class="col-md-12">Natal/RN, 00 de janeiro de 0000</p>
-    </div>
-    <div class="col-md-12"
-    style="margin-top: 34px; font-size: 11pt; font-family: 'Times New Roman'; text-align: center;">
-        <p><b>NOME DA PESSOA AQUI</b></p>
-        <p>Cargo da pessoa aqui</p>
-        <p>Mat 00.00-0</p>
-    </div>
-
 </body>
 
 </html>
