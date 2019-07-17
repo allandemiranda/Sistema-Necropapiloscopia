10ed3b9767022e7d862541143faf072ca154964e
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-07-16 21:53:55 -0300

    nova página

--- perfilDocumento.php
+++ perfilDocumento.php
@@ -270,7 +270,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 }
                                 ?>
                                 <div class="grid1">
-                                    <button type="submit" class="btn btn-1 btn-success">Criar</button>
+                                    <button type="submit" class="btn btn-1 btn-success">Editar</button>
                                     <a href="listaDocumentos.php?page=<?php echo $protocolo; ?>"><button type="button" class="btn btn-1 btn-danger">Voltar</button></a>
                                 </div>
                             </div>
