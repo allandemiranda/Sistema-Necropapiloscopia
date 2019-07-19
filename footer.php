95d501f4bb9e32bf5eb6f09a86e47f3f56464d94
Allan de Miranda <allandemiranda@gmail.com>
2019-07-19 07:52:57 -0300

    dev ver

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!--copy rights start here-->
 <div class="copyrights">
-    <p>© 2019 Sistema Necropapiloscopia. All Rights Reserved | Design by <a href="http://allandemiranda.eti.br/" target="_blank">Allan de Miranda</a> </p>
+    <p>© 2019 Sistema Necropapiloscopia. All Rights Reserved | Design by <a href="http://allandemiranda.eti.br/" target="_blank">Allan de Miranda</a> | v1.3</p>
 </div>
 <!--COPY rights end here-->
